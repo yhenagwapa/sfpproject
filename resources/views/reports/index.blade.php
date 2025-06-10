23bb60d0962a0156f430a02404cd27bdbb8490c8
yhenagwapa <villamil.yhena@gmail.com>
2025-06-10 10:03:15 +0800

    report submenu css change

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -171,20 +171,20 @@
                                                                         <hr class="dropdown-divider">
                                                                     </li>
 
-                                                                    <li class="nav-item">
-                                                                        <label
-                                                                            class="dropdown-item d-flex align-items-center">
-                                                                            <svg class="h-2 w-2 mr-2" width="24"
-                                                                                height="24" viewBox="0 0 24 24"
-                                                                                stroke-width="2" stroke="currentColor"
-                                                                                fill="none" stroke-linecap="round"
-                                                                                stroke-linejoin="round">
-                                                                                <path stroke="none" d="M0 0h24v24H0z" />
-                                                                                <circle cx="12" cy="12"
-                                                                                    r="6" />
-                                                                            </svg>
-                                                                            Age Bracket
-                                                                        </label>
+                                                                    <li class="sub-menu d-flex align-items-center">
+                                                                        <svg class="h-2 w-2 mr-2"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    stroke-width="2"
+                                                                                    stroke="currentColor"
+                                                                                    fill="none"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round">
+                                                                                    <path stroke="none"
+                                                                                        d="M0 0h24v24H0z" />
+                                                                                    <circle cx="12"
+                                                                                        cy="12" r="6" />
+                                                                                </svg>
+                                                                        Age Bracket
                                                                     </li>
                                                                     <li>
                                                                         <hr class="dropdown-divider">
@@ -327,24 +327,20 @@
                                                                             <li>
                                                                                 <hr class="dropdown-divider">
                                                                             </li>
-                                                                            <li>
-                                                                                <label
-                                                                                    class="dropdown-item d-flex align-items-center">
-                                                                                    <svg class="h-2 w-2 mr-2"
-                                                                                        width="24" height="24"
-                                                                                        viewBox="0 0 24 24"
-                                                                                        stroke-width="2"
-                                                                                        stroke="currentColor"
-                                                                                        fill="none"
-                                                                                        stroke-linecap="round"
-                                                                                        stroke-linejoin="round">
-                                                                                        <path stroke="none"
-                                                                                            d="M0 0h24v24H0z" />
-                                                                                        <circle cx="12"
-                                                                                            cy="12" r="6" />
-                                                                                    </svg>
-                                                                                    Undernourish
-                                                                                </label>
+                                                                            <li class="sub-menu d-flex align-items-center">
+                                                                                <svg class="h-2 w-2 mr-2"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    stroke-width="2"
+                                                                                    stroke="currentColor"
+                                                                                    fill="none"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round">
+                                                                                    <path stroke="none"
+                                                                                        d="M0 0h24v24H0z" />
+                                                                                    <circle cx="12"
+                                                                                        cy="12" r="6" />
+                                                                                </svg>
+                                                                                Undernourish
                                                                             </li>
                                                                             <li>
                                                                                 <hr class="dropdown-divider">
@@ -387,25 +383,20 @@
                                                                             <li>
                                                                                 <hr class="dropdown-divider">
                                                                             </li>
-                                                                            <li>
-                                                                                <label
-                                                                                    class="dropdown-item d-flex align-items-center">
-                                                                                    <svg class="h-2 w-2 mr-2"
-                                                                                        width="24" height="24"
-                                                                                        viewBox="0 0 24 24"
-                                                                                        stroke-width="2"
-                                                                                        stroke="currentColor"
-                                                                                        fill="none"
-                                                                                        stroke-linecap="round"
-                                                                                        stroke-linejoin="round">
-                                                                                        <path stroke="none"
-                                                                                            d="M0 0h24v24H0z" />
-                                                                                        <circle cx="12"
-                                                                                            cy="12" r="6" />
-                                                                                    </svg>
-                                                                                    Nutritional Status Upon Entry
-                                                                                </label>
-                                                                            </li>
+                                                                            <li class="sub-menu d-flex align-items-center">
+                                                                                <svg class="h-2 w-2 mr-2"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    stroke-width="2"
+                                                                                    stroke="currentColor"
+                                                                                    fill="none"
+                                                                                    stroke-linecap="round"
+                                                                                    stroke-linejoin="round">
+                                                                                    <path stroke="none"
+                                                                                        d="M0 0h24v24H0z" />
+                                                                                    <circle cx="12"
+                                                                                        cy="12" r="6" />
+                                                                                </svg>
+                                                                                Nutritional Status Upon Entry                                                                            </li>
                                                                             <li>
                                                                                 <hr class="dropdown-divider">
                                                                             </li>
