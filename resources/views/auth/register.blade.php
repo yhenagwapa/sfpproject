462ae3ac696afdb804dcbbf412096b920ab96671
yhenagwapa <villamil.yhena@gmail.com>
2025-07-24 08:30:26 +0800

    added otp for uat

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -336,13 +336,13 @@
                                             </div>
                                         </div>
 
-                                    {{-- <div class="g-recaptcha mt-3 mb-5" data-sitekey="{{ config('services.recaptcha.site_key') }}"></div>
+                                    <div class="g-recaptcha mt-3 mb-5" data-sitekey="{{ config('services.recaptcha.site_key') }}"></div>
 
                                     @if ($errors->has('g-recaptcha-response'))
                                         <span class="text-red-500 text-sm">
                                             {{ $errors->first('g-recaptcha-response') }}
                                         </span>
-                                    @endif --}}
+                                    @endif
 
                                         <div class="flex flex-wrap justify-end w-full md:w-full">
                                             <div class="mt-4">
